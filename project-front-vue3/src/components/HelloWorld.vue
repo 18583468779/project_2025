22dde315804b780de99ffde5c096e34c461368e1
xiewen <1123654054@qq.com>
2025-10-04 18:55:00 +0800

    初始化vue项目

---
+++ project-front-vue3/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>Check out</p>
+  <p>
+    Learn more about IDE Support for Vue in the
+    <a>Vue Docs Scaling up Guide</a>.
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+.card {
+  padding: 2em;
+  border: 10px solid red;
+}
+
+button {
+  border-radius: 10px;
+  background-color: #000;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
---
+++ project-front-vue3/src/index.d.ts
@@ -0,0 +1,2 @@
+declare module "*.css";
+declare module "reset-css"; // 明确声明reset-css模块
