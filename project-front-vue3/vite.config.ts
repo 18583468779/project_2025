44bb28a3e46531c35e946c3ac25082ef2835625f
xiewen <1123654054@qq.com>
2025-10-04 22:58:22 +0800

    继承tailwindcss

--- project-front-vue3/vite.config.ts
+++ project-front-vue3/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue()],
+  plugins: [VueRouter(), vue(), tailwindcss()],
 });
