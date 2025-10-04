10a36133e092d9a8d428e63179471af5a2fe6a2c
xiewen <1123654054@qq.com>
2025-10-05 07:31:44 +0800

    自动引入依赖unplugin-auto-import

--- project-front-vue3/src/pages/index.vue
+++ project-front-vue3/src/pages/index.vue
@@ -4,6 +4,14 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const count = ref(0);
+const user = reactive({ name: "123", age: 123 });
+onMounted(() => {
+  count.value = 100;
+  console.log(count, user);
+});
+console.log(count, user);
+</script>
 
 <style scoped></style>
--- project-front-vue3/vite.config.ts
+++ project-front-vue3/vite.config.ts
@@ -2,7 +2,33 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueRouter from "unplugin-vue-router/vite";
 import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import { VueRouterAutoImports } from "unplugin-vue-router";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), tailwindcss()],
+  plugins: [
+    VueRouter(),
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      dts: "./auto-imports.d.ts",
+      eslintrc: {
+        enabled: true,
+      },
+      // global imports to register
+      imports: [
+        // presets
+        "vue",
+        VueRouterAutoImports,
+      ],
+    }),
+  ],
 });
